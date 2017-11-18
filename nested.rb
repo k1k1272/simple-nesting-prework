9993db4314bbecbdc42399a0d0ee1a30651028b1
Kenny Hom <kenny.hom27@gmail.com>
2017-11-18 22:02:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 
+
 	programmer_hash[:alan_kay][:known_for]
 end
 
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages].last	 
+	programmer_hash[:dennis_ritchie][:languages].last
 end
 
 def adding_matz
